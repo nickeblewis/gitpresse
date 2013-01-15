2c10a09f799007305a74881ac297e8682f01cba9
James Westgate <jamesw@opencomponents.com>
2013-01-15 16:13:28 +0000

    Update scripts/gitpresse.js

---
+++ scripts/gitpresse.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  
+  $(window).load(function() {
+
+    //Load extendify library
+    
+    //Inject the stylesheet into the head
+    
+    //Define the toolbar
+    var toolbar = {div: {'class': 'gitpresse-toolbar'}};
+    
+
+  });
+});
