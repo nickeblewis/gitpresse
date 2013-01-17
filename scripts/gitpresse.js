d2b9aad6f172772647a84de03e9e0de1f445a757
James Westgate <jamesw@opencomponents.com>
2013-01-17 16:55:29 +0000

    Update scripts/gitpresse.js

--- scripts/gitpresse.js
+++ scripts/gitpresse.js
@@ -15,20 +15,13 @@
 *   http://www.gnu.org/licenses/gpl.html
 */
 
-/*
-todo:
-  [ ] add type(object, string) back to extendify
-  [ ] remove deffereds, code events 
-*/
-
 window.extend=function(f){function n(a){return null===a?"null":"undefined"===typeof a?"undefined":Object.prototype.toString.call(a).toLowerCase().replace("[object ","").replace("]","")}function p(a){null!=a&&h[a].open>h[a].closed&&(e.push(">"),h[a].closed++)}function j(a,d){var b=n(a);if("array"===b)a.forEach(function(a){j(a,d)});else if("function"===b)j(a(),d);else if("object"===b)for(var c in a)a.hasOwnProperty(c)&&(b=a[c],h[c]?(p(d),e.push("<"+c),h[c].open++,j(b,c),p(c),e.push("</"+c+">"),result=
 !0):"text"===c?j(b,d):(e.push(" "+c+'="'),j(b,null),e.push('"')));else p(d),e.push(a)}delete window.extend;f=f||window;f.type=function(a,d){return n(a)===d};f.extendify=function(a){return function(d){if(arguments.length){var b=n(d);if("function"===b)d.apply(a);else if("object"===b)for(var c in b)a[c]=b[c]}}};f.namespace=function(a,d){var b=window;if("string"===typeof a)for(var c=a.split("."),m=0,e=c.length;m<e;m++)b=b[c[m]]=b[c[m]]||{},b.extend||(b.extend=f.extendify(b));else b=a;b.extend(d);return b};
 f.define=function(a,d){function b(){for(var b=0,d=c.length;b<d;b++)this.base=a,c[b].apply(this,arguments)}var c=[];1===arguments.length&&(d=a,a=null);a&&(b.prototype=new a);b.prototype.extend=f.extendify(b.prototype);b.extend=function(a){"function"===typeof a&&c.push(a)};b.extend(d);return b};f.load=function(){if(arguments.length)for(var a,d=0,b=0,c=0,f=arguments.length;c<f;c++){var e=arguments[c];if("function"===typeof e){a=e;d=c;b===d&&a(scriptFolder,scriptExt);break}if(loaded[e])b++;else{var g=
 document.createElement("script");g.type="text/javascript";g.onload=g.onreadystatechange=function(){if(g.readyState&&"complete"!==g.readyState&&"loaded"!==g.readyState)return!1;g.onload=g.onreadystatechange=null;b++;a&&b===d&&a()};g.async=!0;g.src=e;document.head.appendChild(g)}}};for(var h={},e,q="abipq bbbrdddldtemh1h2h3h4h5h6hrliolrprttdthtrul bdocoldeldfndivimginskbdmapnavpresubsupvar abbrareabasebodycitecodeformheadhtmllinkmarkmenumetarubysampspantime asideaudioembedinputlabelmeterparamsmalltabletbodytfoottheadtitlevideo buttoncanvasdialogfigurefooterheaderiframelegendobjectoptionoutputscriptselectsourcestrong addressarticlecaptioncommanddetailssection colgroupdatagriddatalistfieldsetnoscriptoptgroupprogresstextarea  blockquote eventsource".split(" "),
 k=1,s=q.length;k<=s;k++)for(var r=q[k-1],l=0,t=r.length;l<t;l+=k)h[r.substring(l,l+k)]={open:0,closed:0};f.compose=function(a){e=[];j(a,null);return e.join("")}};
-
 /*
-* Girpresse' 0.9 Alpha
+* Gitpresse' 0.9 Alpha
 * 
 * Copyright (c) James Westgate 2013
 *
@@ -37,40 +30,45 @@ k=1,s=q.length;k<=s;k++)for(var r=q[k-1],l=0,t=r.length;l<t;l+=k)h[r.substring(l
 *   http://www.gnu.org/licenses/gpl.html
 */
 
-//Check for jQuery
-if (type(window.jQuery, 'undefined')) { 
-  console.log('Gitpresse\' requires jQuery 1.9.0 or newer');
-  return;
-}
+(function() {
   
-//Extend the window namespace with the default Extendify methods 
-extendify(window);
-
-namespace('gitpresse.editing', function() {
- 
-  this.switchbranch = function(branch, reload) {
-    
-  };
-});
-
-
-//Modify the dom with the editing tools
-$(document).ready(function() {
+  //Extend the window namespace with the default Extendify methods 
+  extend(window);
   
-  $(window).load(function() {
+  //Check for jQuery
+  if (type(window.$, 'undefined')) { 
+    console.log('Gitpresse\' requires jQuery 1.9.0 or newer');
+    return;
+  }
 
-    //Inject the stylesheet into the head
-    
-    //Define the toolbar
-    var toolbar = {div: {'class': 'gitpresse-toolbar'}};
-    
-    //Add the toolbar (hidden)
-    
-    //Measure the toolbar, add margin for the toolbar and prepend to the body
-    
-    //Boot the current branch (gh-pages)
-    gitpresse.editing.switchBranch('gh-pages', false);
-  
+  //Public functions
+  namespace('gitpresse.editing', function() {
+   
+    this.switchBranch = function(branch, reload) {
+      
+    };
   });
-});
   
+  
+  //Startup - modify the dom with the editing tools
+  $(document).ready(function() {
+    
+    $(window).load(function() {
+  
+      //Inject the stylesheet into the head
+      
+      //Define the toolbar
+      var toolbar = {div: {'class': 'gitpresse-toolbar gitpresse-invisible'}};
+      
+      //Add the toolbar (hidden)
+      $('body').prepend(compose(toolbar));
+      
+      //Measure the toolbar, add margin for the toolbar and prepend to the body
+      
+      
+      //Boot the current branch (gh-pages)
+      gitpresse.editing.switchBranch('gh-pages', false);
+    
+    });
+  });
+})();  
